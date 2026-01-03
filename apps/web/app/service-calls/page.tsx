ec8cdb73803a555eac3e70346d413c8a509e2089
seedx <v.kolesnik6@students.clark.edu>
2026-01-02 22:53:40 -0800

    Remove dead code: Delete _app.tsx and unused React import
    - Delete apps/web/app/_app.tsx (dead code - not used in Next.js 14 App Router)
    - Remove unused React import from service-calls/page.tsx
    - App Router uses layout.tsx instead of _app.tsx
    - Clean up unused imports

--- apps/web/app/service-calls/page.tsx
+++ apps/web/app/service-calls/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
 import { API_BASE_URL } from '../../lib/config'
